300916e0a7814c9f94d70038cc4b8a20f774800b
HannaBatarchuk <h.batarchuk@gmail.com>
2021-11-24 15:02:46 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -39,7 +39,10 @@ function onInputChange (evt){
 }
 // - очищаємо дані в локальному сховищі та формі
 function onInputFormSub(evt) {
+    evt.preventDefault();
+    evt.currentTarget.reset();
     localStorage.removeItem(LOCALSTORAGE_KEY);
+    console.log('Дані відправлено! Форма очищена.')
 }
 
 // - отримуємо дані зі сховища
@@ -59,6 +62,46 @@ if(persistedForm){
     }   
 }
 
+// перший варіант ДЗ
+// import { throttle } from "lodash";
+// const LOCALSTORAGE_KEY = 'feedback-form-state';
+// const inputForm = document.querySelector('.feedback-form');
+
+// initForm();
+
+// inputForm.addEventListener('input', onFormInputContent);
+// inputForm.addEventListener('submit', onInputFormSub);
+// inputForm.addEventListener('change', throttle(onInputChange, 500));
+
+// function onFormInputContent(evt) {
+    
+//     const formData = new FormData(inputForm);
+//     formData.forEach((value, name) => console.log(value, name));
+// };
+
+// function onInputFormSub(evt) {
+//     evt.preventDefault();
+//     evt.currentTarget.reset();
+//     localStorage.removeItem(LOCALSTORAGE_KEY);
+    
+// }
+
+// function onInputChange (evt){
+//     let persistedForm = localStorage.getItem(LOCALSTORAGE_KEY);
+//     persistedForm = persistedForm ? JSON.parse(persistedForm) : {};
+//     persistedForm[evt.target.name] = evt.target.value;
+//     localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(persistedForm));
+// }
+
+// function initForm(){
+// let persistedForm = localStorage.getItem(LOCALSTORAGE_KEY);
+// if(persistedForm){
+//     persistedForm = JSON.parse(persistedForm);
+//     Object.entries(persistedForm).forEach(([name, value]) => {
+//         inputForm.elements[name].value = value;
+//     });
+// }
+// }
 
 
 // якщо треба зберігати дані лише з одного поля форми
